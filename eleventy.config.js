fb1d4f6854037b707bfbe526047627665bfa75e8
Michel Maillard <info@maillard.li>
2024-09-12 22:28:06 +0200

    changed folder structure

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,4 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets"); // Scss, JS, and images files
-    return {
-        dir: {
-            input: "src", // Set the source for 11ty to the /src directory
-        },
-        templateFormats: ["html", "md", "liquid"]
-    };
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addWatchTarget("assets");
 };
